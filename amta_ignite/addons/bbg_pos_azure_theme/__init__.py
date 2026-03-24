a698fa419d23eef480dfedc74744d764f97a7f83
Mazinsali77 <mazinsali77@gmail.com>
2026-03-24 17:02:15 +0400

    ADD 001

--- amta_ignite/addons/bbg_pos_azure_theme/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# === © BIGBANG - ODOO INIT FILE === #
-# == © BIGBANG ERP - POS AZURE THEME == #
-
-# from . import models
--- amta_ignite/addons/ignite_backend_theme/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# from . import res_config_settings
-# from . import module_installation
-# from . import activity_icons
-# from . import settings_icons
-# from . import apps_icons
-# from . import chatbot_name
-from . import res_users
